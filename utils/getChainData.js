8b80ec2f7d583a0a551fd41253643289f342f8f4
Noah Maizels <noahmaizels@protonmail.com>
2021-03-04 18:43:13 -0700

    Update getChainData.js

--- utils/getChainData.js
+++ utils/getChainData.js
@@ -282,12 +282,12 @@ const getPriceData = async () => {
 
 
 const scheduleUpdates = async (web3s) => {
- const getDataSchedule = schedule.scheduleJob("* 30 * * * *", async () => {   
+ const getDataSchedule = schedule.scheduleJob("* * * * *", async () => {   
    console.log("About to run getData to get token data using Infura") 
     getData(web3s)
   })
   
- const getRangeSchedule = schedule.scheduleJob("* 30 * * * *", async () => { 
+ const getRangeSchedule = schedule.scheduleJob("* * * * *", async () => { 
     console.log("About to run getDataRanges to get token price data from CoinGecko")
     getDataRanges()
   })
